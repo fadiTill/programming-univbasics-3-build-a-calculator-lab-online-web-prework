0c18bf65db8ee932ac8cf896a71197b8f6f8d1c6
fadimata tillman <fadi.tillman@gmail.com>
2019-12-28 08:28:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,26 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-number = 1 -2
+number = (1 -2)
 puts number
 end
 
 def division(num1, num2)
-number = 4 / 2
+number = (4 / 2)
 puts number
 end
 
 def multiplication(num1, num2)
-number = 1 * 2
+number = (1 * 2)
 puts number
 end
 
 def modulo(num1, num2)
-  number = 3 / 2
+  number = (3 / 2)
 puts number
 end
 
 def square_root(num)
-number = 1 ^ 2
+number = (1 ^ 2)
 puts number
 end
